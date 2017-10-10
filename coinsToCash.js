62ebf09a4391e2c8c05dd47d13607a0ff729dbcd
Max Wolf <maxwolf@Maxs-MacBook-Pro.local>
2017-10-09 20:37:01 -0500

    tested some new code and cleaned up the code. final solution to coins to cash

--- coinsToCash.js
+++ coinsToCash.js
@@ -1,25 +1,44 @@
+//defining the object
 const piggyBank = {
     'quarters' : 10,
     'dimes' : 10,
     'nickels' : 10,
     'pennies' : 10
 }
-console.log(piggyBank);
+// console.log(piggyBank);
 
 // for in loop to convert and restore all the coin values to dollar values
-for (let i in piggyBank) {
-    if (i === 'quarters') {
-        piggyBank[i] *= 0.25;
-    } else if ( i === 'dimes') {
-        piggyBank[i] *= 0.1;
-    } else if ( i === 'nickels') {
-        piggyBank[i] *= 0.05;
-    } else {
-        piggyBank[i] *= 0.01;
-    }
+// for (let i in piggyBank) {
+//     if (i === 'quarters') {
+//         piggyBank[i] *= 0.25;
+//     } else if ( i === 'dimes') {
+//         piggyBank[i] *= 0.1;
+//     } else if ( i === 'nickels') {
+//         piggyBank[i] *= 0.05;
+//     } else {
+//         piggyBank[i] *= 0.01;
+//     }
+// }
+
+//trying out a switch statement to do what the if else statment does above
+for (let i in piggyBank)  {
+    switch ( i )  {
+        case 'quarters' :
+            piggyBank[i] *= 0.25;
+            break;
+        case 'dimes' :
+            piggyBank[i] *= 0.1;
+            break;
+        case 'nickels' :
+            piggyBank[i] *= 0.05;
+            break;
+        case 'pennies' :
+            piggyBank[i] *= 0.01;
+            break;
+    }    
 }
 
-console.log(piggyBank);
+// console.log(piggyBank);
 
 // for in loop to add all the values in piggyBank
 let dollarAmount = 0;
@@ -27,5 +46,5 @@ for (let i in piggyBank) {
      dollarAmount += piggyBank[i];
  }
 
- console.log(dollarAmount);
+ console.log("The dollar amount in our piggy bank is $" + dollarAmount);
 
